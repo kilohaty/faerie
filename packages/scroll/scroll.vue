b0c9b9c47b7119b9a2747e235a2a2159366961d9
axi <axi@kilohaty.com>
2017-11-01 15:08:12 +0800

    Add: add dataModel to scroll;

--- packages/scroll/scroll.vue
+++ packages/scroll/scroll.vue
@@ -54,7 +54,12 @@
       elSelector: {
         type: String,
         default: ''
-      }
+      },
+
+      dataModel: {
+        type: Number,
+        default: 0
+      },
     },
 
     computed: {
@@ -64,43 +69,56 @@
       optionProperty() {
         return this.direction === 'vertical' ? 'translateY' : 'translateX';
       },
-      optionMin() {
+      scrollClass() {
+        return this.direction;
+      },
+    },
+
+    watch: {
+      elSelector: function () {
+        if (this.autoCenter && this.elSelector) {
+          this.toCenter();
+        }
+      },
+      dataModel: function () {
+        this.updateScroller();
+      },
+    },
+
+    mounted() {
+      this.updateScroller();
+    },
+
+    methods: {
+      getOptionMin() {
         const scroller = this.$refs.frScrollWrapper;
         return this.direction === 'vertical' ?
           -(scroller.scrollHeight - scroller.offsetHeight)
           : -(scroller.scrollWidth - scroller.offsetWidth);
       },
-      scrollClass() {
-        return this.direction;
-      }
-    },
 
-    watch: {
-      elSelector: function() {
-        if (this.autoCenter && this.elSelector) {
-          this.toCenter();
+      updateScroller() {
+        const scroll          = this.$refs.frScroll;
+        const scrollerWrapper = this.$refs.frScrollWrapper;
+
+        if (!this.alloyTouch) {
+          Transform(scrollerWrapper);
+          this.$nextTick(() => {
+            this.alloyTouch = new AlloyTouch({
+              touch: scroll,
+              target: scrollerWrapper,
+              vertical: this.optionVertical,
+              property: this.optionProperty,
+              min: this.getOptionMin(),
+              max: 0
+            });
+          });
+        } else {
+          this.alloyTouch.min = this.getOptionMin();
         }
-      }
-    },
 
-    mounted() {
-      const scroll          = this.$refs.frScroll;
-      const scrollerWrapper = this.$refs.frScrollWrapper;
+      },
 
-      Transform(scrollerWrapper);
-      this.$nextTick(() => {
-        this.alloyTouch = new AlloyTouch({
-          touch: scroll,
-          target: scrollerWrapper,
-          vertical: this.optionVertical,
-          property: this.optionProperty,
-          min: this.optionMin,
-          max: 0
-        });
-      });
-    },
-
-    methods: {
       toCenter() {
         const el = document.querySelector(this.elSelector);
         if (!el) return;
@@ -112,7 +130,7 @@
         const eleWidth       = el.offsetWidth + parseInt(marginLeft) + parseInt(marginRight);
 
         let x = -(eleLeft - (containerWidth - eleWidth) / 2);
-        x     = x > 0 ? 0 : x < this.optionMin ? this.optionMin : x;
+        x     = x > 0 ? 0 : x < this.getOptionMin() ? this.getOptionMin() : x;
 
         this.alloyTouch.to(x, 300);
       },
